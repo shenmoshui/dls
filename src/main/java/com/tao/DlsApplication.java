680873c97282eba286bbc8318444e3a0dfcad125
tom <shen_moshui@163.com>
2020-03-21 11:47:47 +0800

    init

---
+++ src/main/java/com/tao/DlsApplication.java
@@ -0,0 +1,13 @@
+package com.tao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DlsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DlsApplication.class, args);
+    }
+
+}
